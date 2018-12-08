cb2f5ca618d2fc5489eaec291d18fec8780155be
kashwa <kashwa80@gmail.com>
2018-12-08 17:19:40 +0200

    Publish database and setup Models

---
+++ Modules/HirMVC/Entities/Post.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\HirMVC\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $table = 'posts';
+
+    protected $fillable = [];
+}
---
+++ Modules/HirMVC/Entities/User.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\HirMVC\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'users';
+
+    protected $fillable = [];
+}
